2edb80a770cf41445c319ea6a5a4b2ac1a682c9b
David Vazquez <dave_u@outlook.com>
2024-08-28 22:47:01 -0600

    Add SQL queries for continent and country joins

---
+++ joins.sql
@@ -0,0 +1,49 @@
+
+
+SELECT * FROM continent WHERE name like '%America%'
+UNION
+SELECT * FROM continent WHERE code in (3,5)
+order by name asc;
+
+
+
+select
+    a.name as country,
+    b.name as continent
+from
+    country a,
+    continent b
+where a.continent = b.code
+order by b.name asc;
+
+
+--Inner join
+SELECT a.name as country, b.name as continent FROM country a
+inner JOIN continent b
+on a.continent = b.code
+order by b.name asc;
+
+SELECT * FROM continent WHERE code = 6;
+
+--reiniciar la secuencia al numero que se coloque
+alter SEQUENCE 'sq name' RESTART with 8;
+
+-- country a   -name, continent (codigo numerico)
+-- countinent b -name as continentName
+
+
+SELECT a.name as country , a.continent as continentCode, b.name as continentName
+FROM country a 
+FULL OUTER JOIN continent b ON a.continent = b.code;
+
+
+SELECT a.name as country, b.name as continentName
+FROM country a
+RIGHT JOIN continent b
+ON a.continent = b.code
+WHERE a.continent IS NULL;
+
+
+SELECT count(*), continent from country
+group by continent
+ORDER by continent asc;
